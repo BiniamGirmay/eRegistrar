7616c3b7b0498026dbb871b85266debccdbe7876
BiniamGirmay <bgirmay@miu.edu>
2022-01-28 10:30:34 -0600

    this

---
+++ eregistrar/src/main/java/com/registrar/eregistrar/repository/StudentRepository.java
@@ -0,0 +1,23 @@
+package com.registrar.eregistrar.repository;
+
+import com.registrar.eregistrar.domain.Student;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+
+    @Query("select s from Student s where number = ?1")
+    public List<Student> getStudentByNumber(String number);
+    public abstract List<Student> findAllByNumberOrFirstNameOrMiddleNameOrLastName(String number, String firstName,String middleName , String lastName);
+    public abstract List<Student> findAllByEnrollmentDate(LocalDate enrollmentDate);
+    public abstract List<Student> findAllByCgpa(Double cgpa);
+
+
+}
+
+
---
+++ eregistrar/src/test/java/com/registrar/eregistrar/EregistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registrar.eregistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EregistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
